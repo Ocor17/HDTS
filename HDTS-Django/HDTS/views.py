f1fc40956789d4651f2aa5c1d8b28c29100f4921
Alan Garcia Willars <43868749+Alan1453563@users.noreply.github.com>
2022-02-26 18:45:34 -0700

    Ivan and Alan finished the majority for creating

--- HDTS-Django/HDTS/views.py
+++ HDTS-Django/HDTS/views.py
@@ -4,32 +4,34 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .forms import addNewHardDrive
 
+from django.views.decorators.csrf import csrf_exempt
 #from .models import HardDrive
 
+@csrf_exempt
 def addHardDrive(request):
     if request.method == 'POST':
         form = addNewHardDrive(request.POST)
-        print(form.cleaned_data)
+        #print(form.cleaned_data)
         if form.is_valid():
-            pass
-            """creationDate = form.cleaned_data['']
-            serialNo = form.cleaned_data['']
-            manufacturer = form.cleaned_data['']
-            modelNo = form.cleaned_data['']
-            hdType = form.cleaned_data['']
-            connPort = form.cleaned_data['']
-            hdSize = form.cleaned_data['']
-            hdClass = form.cleaned_data['']
-            justiClass = form.cleaned_data['']
-            imageVerID = form.cleaned_data['']
-            btStatus = form.cleaned_data['']
-            btExpDate = form.cleaned_data['']
-            hdStatus = form.cleaned_data['']
-            justiStatus = form.cleaned_data['']
-            issueDate = form.cleaned_data['']
-            expectRetDate = form.cleaned_data['']
-            justiRetDate = form.cleaned_data['']
-            actualRetDate = form.cleaned_data['']
-            modDate = form.cleaned_data[''] """
+            creationDate = form.cleaned_data['creationDate']
+            serialNo = form.cleaned_data['serialNo']
+            manufacturer = form.cleaned_data['manufacturer']
+            modelNo = form.cleaned_data['modelNo']
+            hdType = form.cleaned_data['hdType']
+            connPort = form.cleaned_data['connPort']
+            hdSize = form.cleaned_data['hdSize']
+            hdClass = form.cleaned_data['hdClass']
+            justiClass = form.cleaned_data['justiClass']
+            imageVerID = form.cleaned_data['imageVerID']
+            btStatus = form.cleaned_data['btStatus']
+            btExpDate = form.cleaned_data['btExpDate']
+            hdStatus = form.cleaned_data['hdStatus']
+            justiStatus = form.cleaned_data['justiStatus']
+            issueDate = form.cleaned_data['issueDate']
+            expectRetDate = form.cleaned_data['expectRetDate']
+            justiRetDate = form.cleaned_data['justiRetDate']
+            actualRetDate = form.cleaned_data['actualRetDate']
+            modDate = form.cleaned_data['modDate'] 
         
+        return HttpResponse('Form Works!')
     return HttpResponse('Hello World!')
