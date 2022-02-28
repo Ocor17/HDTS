de2d2efdf5f03fcc4b04359e08a3bae37cc5a271
Juan Oscar Gutierrez <oscarnerf@gmail.com>
2022-02-27 20:15:34 -0700

    Create utils.py
    MongoDB support added

--- HDTS-Django/HDTS/settings.py
+++ HDTS-Django/HDTS/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'HDTS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'hdts',
+        'CLIENT': {
+            'username': 'team7',
+            'password': 'team7',
+            'host': 'hdts.azemend.com',
+            'port': 4444,
+        }
+
     }
 }
+
 
 
 # Password validation
