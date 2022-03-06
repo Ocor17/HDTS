f3ded22e012fb9f693df5bdea9139902d3b6b71e
Alan Garcia Willars <43868749+Alan1453563@users.noreply.github.com>
2022-03-06 13:35:02 -0700

    Added no Hard Drive dups

--- HDTS-Django/Inventory/views.py
+++ HDTS-Django/Inventory/views.py
@@ -12,46 +12,47 @@ def addHardDrive(request):
         form = addNewHardDrive(request.POST, request.FILES)
         #print(form.cleaned_data)
         if form.is_valid():
-            creationDate = form.cleaned_data['creationDate']
-            serialNo = form.cleaned_data['serialNo']
-            manufacturer = form.cleaned_data['manufacturer']
-            modelNo = form.cleaned_data['modelNo']
-            hdType = form.cleaned_data['hdType']
-            connPort = form.cleaned_data['connPort']
-            hdSize = form.cleaned_data['hdSize']
-            hdClass = form.cleaned_data['hdClass']
-            justiClass = form.cleaned_data['justiClass']
-            imageVerID = form.cleaned_data['imageVerID']
-            btStatus = form.cleaned_data['btStatus']
-            btExpDate = form.cleaned_data['btExpDate']
-            hdStatus = form.cleaned_data['hdStatus']
-            justiStatus = form.cleaned_data['justiStatus']
-            issueDate = form.cleaned_data['issueDate']
-            expectRetDate = form.cleaned_data['expectRetDate']
-            justiRetDate = form.cleaned_data['justiRetDate']
-            actualRetDate = form.cleaned_data['actualRetDate']
-            modDate = form.cleaned_data['modDate'] 
+            if not HardDrive.objects.filter(serialNo = form.cleaned_data['serialNo']).exists():
+                creationDate = form.cleaned_data['creationDate']
+                serialNo = form.cleaned_data['serialNo']
+                manufacturer = form.cleaned_data['manufacturer']
+                modelNo = form.cleaned_data['modelNo']
+                hdType = form.cleaned_data['hdType']
+                connPort = form.cleaned_data['connPort']
+                hdSize = form.cleaned_data['hdSize']
+                hdClass = form.cleaned_data['hdClass']
+                justiClass = form.cleaned_data['justiClass']
+                imageVerID = form.cleaned_data['imageVerID']
+                btStatus = form.cleaned_data['btStatus']
+                btExpDate = form.cleaned_data['btExpDate']
+                hdStatus = form.cleaned_data['hdStatus']
+                justiStatus = form.cleaned_data['justiStatus']
+                issueDate = form.cleaned_data['issueDate']
+                expectRetDate = form.cleaned_data['expectRetDate']
+                justiRetDate = form.cleaned_data['justiRetDate']
+                actualRetDate = form.cleaned_data['actualRetDate']
+                modDate = form.cleaned_data['modDate'] 
 
-            hd = HardDrive.objects.create(creationDate=creationDate, 
-                                          serialNo=serialNo, 
-                                          manufacturer=manufacturer, 
-                                          modelNo=modelNo,
-                                          hdType=hdType, 
-                                          connPort=connPort, 
-                                          hdSize=hdSize, 
-                                          hdClass=hdClass, 
-                                          justiClass=justiClass, 
-                                          imageVerID=imageVerID, 
-                                          btStatus=btStatus,
-                                          btExpDate=btExpDate, 
-                                          hdStatus=hdStatus, 
-                                          justiStatus=justiStatus, 
-                                          issueDate=issueDate, 
-                                          expectRetDate=expectRetDate,
-                                          justiRetDate=justiRetDate, 
-                                          actualRetDate=actualRetDate, 
-                                          modDate=modDate)            #hd.save()
-            return HttpResponse('Form Works!')
+                hd = HardDrive.objects.create(creationDate=creationDate, 
+                                            serialNo=serialNo, 
+                                            manufacturer=manufacturer, 
+                                            modelNo=modelNo,
+                                            hdType=hdType, 
+                                            connPort=connPort, 
+                                            hdSize=hdSize, 
+                                            hdClass=hdClass, 
+                                            justiClass=justiClass, 
+                                            imageVerID=imageVerID, 
+                                            btStatus=btStatus,
+                                            btExpDate=btExpDate, 
+                                            hdStatus=hdStatus, 
+                                            justiStatus=justiStatus, 
+                                            issueDate=issueDate, 
+                                            expectRetDate=expectRetDate,
+                                            justiRetDate=justiRetDate, 
+                                            actualRetDate=actualRetDate, 
+                                            modDate=modDate)            #hd.save()
+                return HttpResponse('Form Works!')
     return HttpResponse('Hello World!')
 
 def viewInventory(request):
