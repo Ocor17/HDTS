343994f48602a8788cd502f501b2993182950f23
Ocor17 <oscarnerf@gmail.com>
2022-05-29 02:41:12 -0600

    secret key change

--- HDTS-Django/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build . -t hdts:latest
-sudo docker-compose up -d
--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 sudo docker build . -t hdts:latest
 sudo docker network create hdts_net
 sudo docker-compose up -d
+
